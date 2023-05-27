c22fa57fe438b25c86406ec6e211e7dbb2d3b220
Fatih Yurek <fatih_yurek@emrisoft.com>
2023-05-27 13:08:46 +0300

    feat: Input makes dynamic for reservation section

--- components/form/Input.jsx
+++ components/form/Input.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
-const Input = () => {
+const Input = (props) => {
+  const { type, placeholder, ...inputProps } = props;
   return (
     <div className="w-full">
       <label className="relative block w-full cursor-text">
         <input
-          type="text"
-          className="peer h-14 w-full border border-primary px-4 pt-2 outline-none"
+          type={type}
+          className={`peer h-14 w-full border border-primary px-4 outline-none ${
+            type !== "datetime-local" && "pt-2"
+          }`}
           required
+          {...inputProps}
         />
-        <span className="absolute left-0 top-0 flex h-full items-center px-4 text-sm transition-all peer-valid:h-7 peer-valid:text-xs peer-focus:h-7 peer-focus:text-xs">
-          Email
-        </span>
+        {type !== "datetime-local" && (
+          <span className="absolute left-0 top-0 flex h-full items-center px-4 text-sm transition-all peer-valid:h-7 peer-valid:text-xs peer-focus:h-7 peer-focus:text-xs">
+            {placeholder}
+          </span>
+        )}
       </label>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import Carousel from "../../components/Carousel";
 import Campaigns from "../../components/Campaigns";
 import MenuWrapper from "../../components/product/MenuWrapper";
 import About from "../../components/About";
-import Input from "../../components/form/Input";
 import Reservation from "../../components/Reservation";
 
 export default function Index() {
